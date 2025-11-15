08e99de4aeb7c9ce23a8289be81325eb75e8a9a1
Matt Ayers <73685857+mattayersnz@users.noreply.github.com>
2025-11-15 19:41:00 +0100

    Initial Commit

---
+++ beats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  beats
+//
+//  Created by Matt Ayers on 15/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ beats/beatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  beatsApp.swift
+//  beats
+//
+//  Created by Matt Ayers on 15/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct beatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
